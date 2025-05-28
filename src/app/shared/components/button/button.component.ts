c75fa5c94eaaab2752e497f6ec895a2f452aa240
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 15:56:01 -0500

    feat: añadir ButtonComponent con plantilla, estilos y pruebas - 28/05/2025

---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

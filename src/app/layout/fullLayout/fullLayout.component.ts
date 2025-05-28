5bf8c98adb973a1bef151a9600c0b721e96bf1d7
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 11:40:09 -0500

    feat: integrar los componentes de cabecera y pie de página en FullLayout - 28/05/2025

--- src/app/layout/fullLayout/fullLayout.component.ts
+++ src/app/layout/fullLayout/fullLayout.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   selector: 'app-fullLayout',
   templateUrl: './fullLayout.component.html',
   styleUrls: ['./fullLayout.component.css']

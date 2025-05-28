7149bbb673d6d6d1635ae1c306113968c715f61f
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 14:54:24 -0500

    feat: refactorizar AuthService para utilizar HttpClient para llamadas API y gestionar sesiones de usuario - 28/05/2025

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,10 +1,71 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, of, map, catchError } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { UserModel, RoleModel } from '../../shared/models';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-constructor() { }
+  private http = inject(HttpClient);
+  private readonly apiURL: string = environment.apiUrl;
+  private readonly USER_KEY = 'auth_user';
+
+  
+  // Método para iniciar sesión
+  login(username: string, password: string): Observable<UserModel | null> {
+    const url = `${this.apiURL}/users/username=${username}&password=${password}`;
+
+    return this.http.get<UserModel[]>(url)
+    .pipe(
+      map(users => users.length > 0 ? users[0] : null),
+      catchError( () => of(null))
+    );
+  }
+
+  // Método para obtener los roles del usuario
+  getRoles(): Observable<RoleModel[]>{
+    const url = `${this.apiURL}/roles`;
+
+    return this.http.get<RoleModel[]>(url)
+  }
+
+
+  // Método para guardar el usuario autenticado en el localStorage
+  setSessionUser(user: UserModel){
+    localStorage.setItem(this.USER_KEY, JSON.stringify(user));
+  }
+
+
+  // Método para obtener el usuario autenticado desde el localStorage
+  getSessionUser(): UserModel | null{
+    const user = localStorage.getItem(this.USER_KEY);
+    return user ? JSON.parse(user) : null;
+  }
+
+
+  // Método para cerrar sesión
+  logout(): void {
+    localStorage.removeItem(this.USER_KEY);
+  }
+
+
+  // Método para redireccionar al usuario según su rol
+  redirectByRoleUser(role: string, router: Router) {
+
+    if(role === 'analyst'){
+      router.navigate(['/analysts']);
+    } else if(role === 'manager'){
+      router.navigate(['/sales-management'])
+    } else {
+      router.navigate(['/login'])
+    }
+
+  }
 
 }

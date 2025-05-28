71688b7526746df2f15691b8a45b60be652d3f6e
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 16:23:31 -0500

    feat: correcion de implementacion de Input - 28/05/2025

--- src/app/shared/components/custom-input/custom-input.component.ts
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, forwardRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({

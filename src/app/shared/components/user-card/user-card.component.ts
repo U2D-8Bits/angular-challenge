7abcd34e196e877d90bff0eb3b26a7741db618a8
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 10:39:27 -0500

    shared: añade componente UserCard con su plantilla, estilos y pruebas unitarias - 28/05/2025

---
+++ src/app/shared/components/user-card/user-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-user-card',
+  templateUrl: './user-card.component.html',
+  styleUrls: ['./user-card.component.css']
+})
+export class UserCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

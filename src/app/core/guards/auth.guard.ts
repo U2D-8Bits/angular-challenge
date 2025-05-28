1aadf7ecad63d7858138bab40ccd77697595639d
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 14:54:35 -0500

    feat: implementar lógica de guardias de autenticación y roles en el enrutador - 28/05/2025

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,28 @@
 import type { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getSessionUser();
+
+
+  // Redirigir a "dashboard" si el usuario ya esta autenticado
+  if(state.url === '/login' && user){
+    authService.redirectByRoleUser(user.role, router)
+    return false;
+  }
+
+
+  // Redirigir al login si el usuario no esta autenticado
+  if(!user && state.url !== '/login'){
+    router.navigate(['/login']);
+    return false
+  }
+
   return true;
 };
--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,5 +1,30 @@
 import type { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
 
 export const roleGuard: CanActivateFn = (route, state) => {
-  return true;
+  
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const user = authService.getSessionUser();
+
+
+  // Obtenemos el rol permitido
+  const allowedRoles = route.data?.['roles'] as string[] | undefined;
+  
+  if(user && allowedRoles && allowedRoles.includes(user.role)){
+    return true;
+  }
+
+  // Si el usuario no tiene el rol correspondiente redirigirlo a su ruta
+  if(user){
+    authService.redirectByRoleUser(user.role, router);
+  }else {
+    router.navigate(['/login'])
+  }
+
+  return false;
 };

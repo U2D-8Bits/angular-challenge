abcaa54b7ef42edac569f3483ed1a0d209baf068
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 10:38:33 -0500

    Shared: añade componente ProductSelector con su plantilla y estilos - 28/05/2025

---
+++ src/app/shared/components/product-selector/product-selector.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-product-selector',
+  templateUrl: './product-selector.component.html',
+  styleUrls: ['./product-selector.component.css']
+})
+export class ProductSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

637ee4265d98e7ba83f9a5e1f0775af6c262a2ca
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 14:06:11 -0500

    feat: añadir componente de input personalizado con plantilla, estilos y pruebas - 28/05/2025

---
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.css']
+})
+export class CustomInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

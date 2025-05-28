b0b2eba470a910af4e8f3493ad1deb113307028d
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 11:08:13 -0500

    features: añade rutas para la autenticación y redirección en la aplicación - 28/05/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { SimpleLayoutComponent } from './layout/simpleLayout/simpleLayout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'login',
+        component: SimpleLayoutComponent,
+        loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+
+];

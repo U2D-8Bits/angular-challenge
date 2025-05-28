0c8e181fd436005de8786fd50fa5fb5c1c221d1f
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 16:23:43 -0500

    feat: implementar ButtonComponent con funcionalidad y estilos - 28/05/2025

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
 
-  constructor() { }
+  @Input() type: 'button' | 'submit' = 'button';
+  @Input() disabled: boolean = false;
+  @Input() loading: boolean = false;
+  @Input() loadingText: string = 'Cargando...';
+  @Input() color: string = '#1877f2';
+  @Input() hoverColor: string = '#166fe5'
+  @Input() textColor: string = '#fff'
 
-  ngOnInit() {
-  }
+
+  hovering = false;
 
 }

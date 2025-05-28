54a3aae4a2363a7d67e768888b790c115ad9266a
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 18:52:28 -0500

    feat: implementar el componente SalesManagement con el componente Card y mejorar la estructura del layout - 28/05/2025

--- src/app/features/sales-management/sales-management.component.ts
+++ src/app/features/sales-management/sales-management.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   selector: 'app-sales-management',
   templateUrl: './sales-management.component.html',
   styleUrls: ['./sales-management.component.css']

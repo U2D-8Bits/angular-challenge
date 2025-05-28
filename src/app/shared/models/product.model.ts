a0f8b549640b34f50ea9b75e383a6f308bdeaf7d
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 10:54:44 -0500

    features: añade modelo ProductModel con sus propiedades - 28/05/2025

---
+++ src/app/shared/models/product.model.ts
@@ -0,0 +1,8 @@
+export interface ProductModel {
+    id:                   number;
+    name:                 string;
+    listPrice:            number;
+    minPromotionQuantity: number;
+    maxPromotionQuantity: number;
+    minPromotionPrice:    number;
+}

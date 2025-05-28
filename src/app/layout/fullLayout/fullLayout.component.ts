c594d454968a487726f70f4eeecd86f11253f02c
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 11:31:35 -0500

    features:  - refactorización del componente FullLayout para incluir la estructura de diseño y la salida del enrutador - 28/05/2025

--- src/app/layout/fullLayout/fullLayout.component.ts
+++ src/app/layout/fullLayout/fullLayout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
+  imports: [RouterOutlet],
   selector: 'app-fullLayout',
   templateUrl: './fullLayout.component.html',
   styleUrls: ['./fullLayout.component.css']

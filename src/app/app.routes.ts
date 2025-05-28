b6270f5a4fdb0d700c49223844f1ebe564e4111e
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 11:24:10 -0500

    features: añade rutas para los componentes Analysts y SalesManagement con sus títulos y estructura - 28/05/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,38 @@
 import { Routes } from '@angular/router';
 import { SimpleLayoutComponent } from './layout/simpleLayout/simpleLayout.component';
+import { FullLayoutComponent } from './layout/fullLayout/fullLayout.component';
 
 export const routes: Routes = [
 
     {
         path: 'login',
+        title: 'Inicio de sesión',
         component: SimpleLayoutComponent,
         children: [
             {
                 path: '',
                 loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent)
+            }
+        ]
+    },
+    {
+        path: 'analysts',
+        title: 'Analistas',
+        component: FullLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./features/analysts/analysts.component').then(m => m.AnalystsComponent)
+            }
+        ]
+    },
+    {
+        path: 'sales-management',
+        title: 'Gestión de ventas',
+        component: FullLayoutComponent,
+        children: [
+            {
+                loadComponent: () => import('./features/sales-management/sales-management.component').then(m => m.SalesManagementComponent)
             }
         ]
     },

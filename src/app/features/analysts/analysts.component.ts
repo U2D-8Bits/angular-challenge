77440b974a769376a768ab853ed7a6f3a3d8fdfc
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 18:51:39 -0500

    feat: actualizar el componente Analysts para incluir el componente Card y mejorar la estructura del layout - 28/05/2025

--- src/app/features/analysts/analysts.component.ts
+++ src/app/features/analysts/analysts.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../../shared/components/card/card.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    CardComponent
+  ],
   selector: 'app-analysts',
   templateUrl: './analysts.component.html',
   styleUrls: ['./analysts.component.css']

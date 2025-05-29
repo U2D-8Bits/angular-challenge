0edb8bf0e4a9b84da3952dbe3eb008e6acc55543
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 20:17:54 -0500

    feat: agregar componente PromotionCard en AnalystsComponent y actualizar la plantilla - 28/05/2025

--- src/app/features/analysts/analysts.component.ts
+++ src/app/features/analysts/analysts.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CardComponent } from '../../shared/components/card/card.component';
+import { PromotionCardComponent } from '../../shared/components/promotionCard/promotionCard.component';
 
 @Component({
   standalone: true,
   imports: [
-    CardComponent
+    CardComponent,
+    PromotionCardComponent
   ],
   selector: 'app-analysts',
   templateUrl: './analysts.component.html',

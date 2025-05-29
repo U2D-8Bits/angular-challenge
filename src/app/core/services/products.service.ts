73352996ee63fcaf657fe2154e1e9d15e5ab8e59
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 19:21:18 -0500

    feat: refactorizar ProductsService para utilizar HttpClient para obtener productos - 28/05/2025

--- src/app/core/services/products.service.ts
+++ src/app/core/services/products.service.ts
@@ -1,10 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { ProductModel } from '../../shared/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-constructor() { }
+  private http = inject(HttpClient);
+  private readonly apiUrl: string = environment.apiUrl;
+
+  // Método para obtener todos los productos
+  getAllProducts(): Observable<ProductModel[]>{
+    return this.http.get<ProductModel[]>(`${this.apiUrl}/products`);
+  }
 
 }

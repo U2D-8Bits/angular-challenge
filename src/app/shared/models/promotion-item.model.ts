b0b25cb21bbe6bf477478b1227d86785024377a0
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 19:20:51 -0500

    feat: agregar modelo PromotionItem para gestionar promociones de productos - 28/05/2025

---
+++ src/app/shared/models/promotion-item.model.ts
@@ -0,0 +1,10 @@
+export interface PromotionItemModel {
+  productId: number;
+  productName: string;
+  listPrice: number;
+  quantity: number;
+  promotionPrice: number;
+  minPromotionQuantity: number;
+  maxPromotionQuantity: number;
+  minPromotionPrice: number;
+}

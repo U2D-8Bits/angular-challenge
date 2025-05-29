c8529bf4fbfe7d78e45814f56ccc5ccd9f7455ac
U2D-8Bits <u2d.8bits@gmail.com>
2025-05-28 19:21:35 -0500

    feat: refactorizar PromotionsService para utilizar inyección de dependencia para StorageService y mejorar los métodos de gestión de promociones - 28/05/2025

--- src/app/core/services/promotions.service.ts
+++ src/app/core/services/promotions.service.ts
@@ -1,10 +1,47 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { PromotionItemModel } from '../../shared/models';
+import { StorageService } from './storage.service';
+
+
+export type PromotionListStatus = 'EDICION' | 'APROBACION' | 'APROBADO'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionsService {
 
-constructor() { }
+  private storage = inject(StorageService);
+  private readonly PROMO_LIST_KEY = 'promotion_list';
+  private readonly PROMO_STATUS_KEY = 'promotion_list_status';
+
+
+  // Método para obtener la lista de promociones desde el LocalStorage
+  getPromotionList(): PromotionItemModel[]{
+    const data = this.storage.getItem(this.PROMO_LIST_KEY);
+    return data ? JSON.parse(data) : [];
+  }
+
+  // Método para guardar la lista de promociones
+  savePromotionList(list: PromotionItemModel[]): void {
+    this.storage.setItem(this.PROMO_LIST_KEY, JSON.stringify(list));
+  }
+
+  // Método para Eliminar la lista de promociones
+  clearPromotionList(): void {
+    this.storage.removeItem(this.PROMO_LIST_KEY);
+    this.storage.removeItem(this.PROMO_STATUS_KEY);
+  }
+
+  // Método para obtener el estado de la lista (EDICION | APROBACION | APROBADO)
+  getPromotionStatus(): PromotionListStatus{
+    return (
+      this.storage.getItem(this.PROMO_STATUS_KEY) as PromotionListStatus
+    ) || 'EDICION';
+  }
+
+  // Método para guardar el estado de la lista
+  setPromotionStatus(status: PromotionListStatus): void {
+    this.storage.setItem(this.PROMO_STATUS_KEY, status);
+  }
 
 }
